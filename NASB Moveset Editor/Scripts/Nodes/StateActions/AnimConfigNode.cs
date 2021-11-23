c591c2568dc5379a253b24b54789efe996b377f9
Megalon <megalongaming@gmail.com>
2021-11-23 03:45:51 -0800

    Generated AnimConfigNode script

--- NASB Moveset Editor/Scripts/Nodes/StateActions/AnimConfigNode.cs
+++ NASB Moveset Editor/Scripts/Nodes/StateActions/AnimConfigNode.cs
@@ -22,11 +22,50 @@ using NASB_Moveset_Editor.Jumps;
 using NASB_Moveset_Editor.CheckThings;
 using NASB_Moveset_Editor.StateActions;
 using NASB_Moveset_Editor.ObjectSources;
+using static NASB_Parser.StateActions.AnimConfig;
 
 namespace NASB_Moveset_Editor.StateActions
 {
 	public class AnimConfigNode : BaseMovesetNode
 	{
+		public float Rate;
+		public float Weight;
+		public NASB_Parser.StateActions.AnimConfig.WrapMode Wrap;
+		public bool ClingToFrames;
+		
+		protected override void Init()
+		{
+			base.Init();
+		}
+		
+		public override object GetValue(NodePort port)
+		{
+			return null;
+		}
+		
+		public int SetData(AnimConfig data, MovesetGraph graph, string assetPath, Vector2 nodeDepthXY)
+		{
+			name = NodeEditorUtilities.NodeDefaultName(typeof(AnimConfig));
+			position.x = nodeDepthXY.x * Consts.NodeXOffset;
+			position.y = nodeDepthXY.y * Consts.NodeYOffset;
+			int variableCount = 0;
+			
+			Rate = data.Rate;
+			Weight = data.Weight;
+			Wrap = data.Wrap;
+			ClingToFrames = data.ClingToFrames;
+			return variableCount;
+		}
+		
+		public AnimConfig GetData()
+		{
+			AnimConfig objToReturn = new AnimConfig();
+			objToReturn.Rate = Rate;
+			objToReturn.Weight = Weight;
+			objToReturn.Wrap = Wrap;
+			objToReturn.ClingToFrames = ClingToFrames;
+			return objToReturn;
+		}
 	}
 }
 
