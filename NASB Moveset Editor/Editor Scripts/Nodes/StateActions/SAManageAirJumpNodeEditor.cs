6cb84f6300498c53e4cbe088c5408273a2060708
Megalon <megalongaming@gmail.com>
2021-11-23 13:02:13 -0800

    SAManageAirJumpNode styling

---
+++ NASB Moveset Editor/Editor Scripts/Nodes/StateActions/SAManageAirJumpNodeEditor.cs
@@ -0,0 +1,49 @@
+using NASB_Moveset_Editor.StateActions;
+using System.Linq;
+using UnityEditor;
+using XNodeEditor;
+
+namespace NASB_Moveset_Editor
+{
+    [CustomNodeEditor(typeof(SAManageAirJumpNode))]
+    public class SAManageAirJumpNodeEditor : NodeEditor
+    {
+
+        bool initialized = false;
+        public override void OnBodyGUI()
+        {
+            serializedObject.Update();
+            string[] excludes = { "m_Script", "graph", "position", "ports" };
+
+            SerializedProperty iterator = serializedObject.GetIterator();
+            bool enterChildren = true;
+            while (iterator.NextVisible(enterChildren))
+            {
+                enterChildren = false;
+                if (excludes.Contains(iterator.name)) continue;
+
+                if (iterator.type.Equals("Enum"))
+                {
+                    iterator.intValue = EditorGUILayout.Popup(iterator.intValue, iterator.enumDisplayNames);
+                } else if (iterator.type.Equals("string"))
+                {
+                    EditorGUILayout.LabelField(iterator.displayName);
+                    iterator.stringValue = EditorGUILayout.TextField(iterator.stringValue);
+                }
+                else
+                {
+                    NodeEditorGUILayout.PropertyField(iterator, true);
+                }
+            }
+
+            // Iterate through dynamic ports and draw them in the order in which they are serialized
+            foreach (XNode.NodePort dynamicPort in target.DynamicPorts)
+            {
+                if (NodeEditorGUILayout.IsDynamicPortListPort(dynamicPort)) continue;
+                NodeEditorGUILayout.PortField(dynamicPort);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
