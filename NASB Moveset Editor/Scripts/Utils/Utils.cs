ff28a2544f5a3a1b7daa692f54ff4d12b6cc97ac
Megalon <megalongaming@gmail.com>
2021-11-23 02:13:49 -0800

    Add GetNodesDirPath to Utils

--- NASB Moveset Editor/Scripts/Utils/Utils.cs
+++ NASB Moveset Editor/Scripts/Utils/Utils.cs
@@ -11,9 +11,29 @@ namespace NASB_Moveset_Editor
     {
         public static string GetGraphsDirPath()
         {
-            var scriptFromDB = AssetDatabase.FindAssets("t:Script GraphHandler");
-            var mainDir = Path.GetDirectoryName(Path.GetDirectoryName(AssetDatabase.GUIDToAssetPath(scriptFromDB[0])));
+            var mainDir = Path.GetDirectoryName(Path.GetDirectoryName(GetAssetPathFromDB("GraphHandler")));
             return Path.Combine(mainDir, "Graphs");
+        }
+
+        public static string GetNodesDirPath()
+        {
+            var mainDir = Path.GetDirectoryName(GetAssetPathFromDB("BaseMovesetNode"));
+            return mainDir;
+        }
+
+        private static string GetAssetPathFromDB(string scriptName)
+        {
+            var scriptsFromDB = AssetDatabase.FindAssets($"t:Script {scriptName}");
+            var scriptPath = AssetDatabase.GUIDToAssetPath(scriptsFromDB[0]);
+            foreach (string scriptGUID in scriptsFromDB)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(scriptGUID);
+                if (Path.GetFileNameWithoutExtension(path).Equals(scriptName))
+                {
+                    scriptPath = path;
+                }
+            }
+            return scriptPath;
         }
     }
 
