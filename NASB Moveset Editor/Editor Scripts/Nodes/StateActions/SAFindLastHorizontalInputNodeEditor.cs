a5931e71260411c2f26f9943cef706819c8b636c
Megalon <megalongaming@gmail.com>
2021-11-23 12:57:10 -0800

    SAFindLastHorizontalInput styling

---
+++ NASB Moveset Editor/Editor Scripts/Nodes/StateActions/SAFindLastHorizontalInputNodeEditor.cs
@@ -0,0 +1,50 @@
+using NASB_Moveset_Editor.StateActions;
+using System.Linq;
+using UnityEditor;
+using XNodeEditor;
+
+namespace NASB_Moveset_Editor
+{
+    [CustomNodeEditor(typeof(SAFindLastHorizontalInputNode))]
+    public class SAFindLastHorizontalInputNodeEditor : NodeEditor
+    {
+
+        bool initialized = false;
+        public override void OnBodyGUI()
+        {
+            serializedObject.Update();
+            string[] excludes = { "m_Script", "graph", "position", "ports" };
+
+            SerializedProperty iterator = serializedObject.GetIterator();
+            bool enterChildren = true;
+            while (iterator.NextVisible(enterChildren))
+            {
+                enterChildren = false;
+                if (excludes.Contains(iterator.name)) continue;
+
+                if (iterator.type.Equals("Enum"))
+                {
+                    EditorGUILayout.LabelField(iterator.displayName);
+                    iterator.intValue = EditorGUILayout.Popup(iterator.intValue, iterator.enumDisplayNames);
+                } else if (iterator.type.Equals("int"))
+                {
+                    EditorGUILayout.LabelField(iterator.displayName);
+                    iterator.intValue = EditorGUILayout.IntField(iterator.intValue);
+                }
+                else
+                {
+                    NodeEditorGUILayout.PropertyField(iterator, true);
+                }
+            }
+
+            // Iterate through dynamic ports and draw them in the order in which they are serialized
+            foreach (XNode.NodePort dynamicPort in target.DynamicPorts)
+            {
+                if (NodeEditorGUILayout.IsDynamicPortListPort(dynamicPort)) continue;
+                NodeEditorGUILayout.PortField(dynamicPort);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
