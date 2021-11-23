57522139ab8a56d240a6f52c9d62bb5f1265aec6
Megalon <megalongaming@gmail.com>
2021-11-23 03:09:33 -0800

    Test script to generate graph with all nodes

---
+++ NASB Moveset Editor/Scripts/NSABMovesetEditorTesting.cs
@@ -0,0 +1,124 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using XNodeEditor;
+using XNode;
+
+namespace NASB_Moveset_Editor
+{
+    public class NSABMovesetEditorTesting : EditorWindow
+    {
+        private static int xSpacing = 240;
+        private static int ySpacing = 140;
+        private static int numPerRow = 6;
+        private static int fileCount = 0;
+
+        [MenuItem("Window/NASB Moveset Editor Testing")]
+        private static void OpenWindow()
+        {
+            NSABMovesetEditorTesting window = GetWindow<NSABMovesetEditorTesting>();
+            window.titleContent = new GUIContent("TESTING NASB Moveset Editor");
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginHorizontal();
+            {
+                GUILayout.FlexibleSpace();
+                EditorGUILayout.BeginVertical(GUILayout.MaxWidth(400));
+                {
+                    EditorGUILayout.Space(10);
+                    EditorGUILayout.HelpBox("Test scripts for the NASB Moveset Editor", MessageType.Warning);
+                    EditorGUILayout.Space(10);
+                    if (GUILayout.Button("Generate AllNodes Graph", GUILayout.MinHeight(35)))
+                    {
+                        GenerateAllNodes();
+                    }
+                    EditorGUILayout.Space(10);
+                }
+                EditorGUILayout.EndVertical();
+                GUILayout.FlexibleSpace();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void GenerateAllNodes()
+        {
+            string graphsFolderPath = Utils.GetGraphsDirPath();
+            if (!Directory.Exists(graphsFolderPath))
+                Directory.CreateDirectory(graphsFolderPath);
+
+            string nodesDirPath = Utils.GetNodesDirPath();
+            string[] directories = Directory.GetDirectories(nodesDirPath);
+            string[] looseFiles = Directory.GetFiles(nodesDirPath);
+
+            fileCount = 0;
+
+            ScriptableObject graph = null;
+            graph = (ScriptableObject)AssetDatabase.LoadMainAssetAtPath($"{graphsFolderPath}/AllNodes.asset");
+
+            if (graph == null)
+            {
+                graph = ScriptableObject.CreateInstance("MovesetGraph");
+                AssetDatabase.CreateAsset(graph, $"{graphsFolderPath}/AllNodes.asset");
+            } else
+            {
+                Debug.Log("Clearing graph...");
+                ((MovesetGraph)graph).Clear();
+            }
+
+            foreach (string filePath in looseFiles)
+            {
+                AddNodeToGraph((MovesetGraph)graph, filePath);
+            }
+            AddSpacingRow();
+
+            foreach (string dir in directories)
+            {
+                string[] files = Directory.GetFiles(dir);
+                foreach (string filePath in files)
+                {
+                    AddNodeToGraph((MovesetGraph)graph, filePath);
+                }
+                AddSpacingRow();
+            }
+        }
+
+        private void AddNodeToGraph(MovesetGraph graph, string filePath)
+        {
+            var extension = Path.GetExtension(filePath);
+            if (!extension.Equals(".cs")) return;
+
+            var filename = filePath.Substring(filePath.IndexOf("NASB Moveset Editor\\Scripts\\Nodes"));
+            filename = filename.Substring("NASB Moveset Editor\\Scripts\\Nodes".Length + 1).Replace("\\", ".");
+            filename = filename.Substring(0, filename.LastIndexOf("."));
+            var t = System.Type.GetType("NASB_Moveset_Editor." + filename);
+
+            // Skip null types
+            if (t == null)
+            {
+                return;
+            }
+
+            Node node = ((MovesetGraph)graph).AddNode(t);
+
+            Vector2 pos = new Vector2((fileCount % numPerRow) * xSpacing, Mathf.Floor(fileCount / numPerRow) * ySpacing);
+
+            node.position = pos;
+            if (filename.IndexOf(".") > 0) {
+                node.name = filename.Substring(filename.LastIndexOf(".") + 1);
+            } else
+            {
+                node.name = filename;
+            }
+
+            ++fileCount;
+        }
+
+        private void AddSpacingRow()
+        {
+            fileCount = (int)(Mathf.Floor(fileCount / numPerRow) + 2) * numPerRow;
+        }
+    }
+
+}
