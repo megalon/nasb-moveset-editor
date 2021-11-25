1d0da78fce8c3e3f066bd4bcf5d55a3cee97289d
Megalon <megalongaming@gmail.com>
2021-11-24 20:04:17 -0800

    Add graph handler scripts to namespace

--- NASB Moveset Editor/Scripts/GraphHandler.cs
+++ NASB Moveset Editor/Scripts/GraphHandler.cs
@@ -1,151 +1,148 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using NASB_Parser;
 using System.IO;
-using System.Threading.Tasks;
-using XNode;
-using NASB_Moveset_Editor;
-using XNodeEditor;
 using System;
 
-public class GraphHandler
+namespace NASB_Moveset_Editor
 {
-    public static void LoadTextAsset()
+    public class GraphHandler
     {
-        string filePath = EditorUtility.OpenFilePanel("Select Text Asset", "", "txt");
-
-        if (filePath == null || filePath.Equals(string.Empty))
+        public static void LoadTextAsset()
         {
-            return;
+            string filePath = EditorUtility.OpenFilePanel("Select Text Asset", "", "txt");
+
+            if (filePath == null || filePath.Equals(string.Empty))
+            {
+                return;
+            }
+
+            BulkSerializeReader ser;
+            using (var fsread = File.OpenRead(filePath))
+                ser = new BulkSerializeReader(fsread);
+            var data = new SerialMoveset(ser);
+
+            var textAssetName = Path.GetFileNameWithoutExtension(filePath);
+
+            GenerateGraphs(data, textAssetName);
+
+            string assetFolderPath = Path.Combine(Utils.GetGraphsDirPath(), textAssetName);
+
+            EditorUtility.DisplayDialog("NASB Moveset Editor", $"Imported {textAssetName}!\n\nYou can find the moveset state graphs in\n\"{assetFolderPath}\"", "OK");
         }
 
-        BulkSerializeReader ser;
-        using (var fsread = File.OpenRead(filePath))
-            ser = new BulkSerializeReader(fsread);
-        var data = new SerialMoveset(ser);
-
-        var textAssetName = Path.GetFileNameWithoutExtension(filePath);
-
-        GenerateGraphs(data, textAssetName);
-
-        string assetFolderPath = Path.Combine(Utils.GetGraphsDirPath(), textAssetName);
-
-        EditorUtility.DisplayDialog("NASB Moveset Editor", $"Imported {textAssetName}!\n\nYou can find the moveset state graphs in\n\"{assetFolderPath}\"", "OK");
-    }
-
-    public static void SaveTextAsset(string graphDir, string graphName)
-    {
-        if (graphDir == null || graphDir.Equals(string.Empty)) return;
-
-        string outputFilePath = EditorUtility.SaveFilePanel("Select Save Location", "", $"{graphName}_new", "txt");
-
-        if (outputFilePath == null || outputFilePath.Equals(string.Empty))
+        public static void SaveTextAsset(string graphDir, string graphName)
         {
-            return;
-        }
+            if (graphDir == null || graphDir.Equals(string.Empty)) return;
 
-        SerialMoveset serialMoveset = new SerialMoveset();
+            string outputFilePath = EditorUtility.SaveFilePanel("Select Save Location", "", $"{graphName}_new", "txt");
+
+            if (outputFilePath == null || outputFilePath.Equals(string.Empty))
+            {
+                return;
+            }
+
+            SerialMoveset serialMoveset = new SerialMoveset();
 
 
-        string[] graphPaths = Directory.GetFiles(graphDir);
-        foreach (string graphPath in graphPaths)
-        {
+            string[] graphPaths = Directory.GetFiles(graphDir);
+            foreach (string graphPath in graphPaths)
+            {
+                try
+                {
+                    // Skip anything that isn't an asset file
+                    if (!graphPath.EndsWith(".asset")) continue;
+
+                    Debug.Log($"Reading graph...\n{graphPath}");
+
+                    MovesetGraph graph = (MovesetGraph)AssetDatabase.LoadMainAssetAtPath(graphPath);
+                    IdStateNode idStateNode = (IdStateNode)graph.nodes.Find(x => x.GetType() == typeof(IdStateNode));
+
+                    serialMoveset.States.Add(idStateNode.GetData());
+                }
+                catch (Exception e)
+                {
+                    var idStateName = Path.GetFileNameWithoutExtension(graphPath);
+
+                    Debug.LogError($"Error reading graph: {idStateName}\n{graphPath}");
+                    Debug.LogError(e.Message);
+
+                    EditorUtility.DisplayDialog("NASB Moveset Editor", $"Export failed!\n\nError with IdState: {idStateName}\n\nSee error message in console window for more info.", "Close");
+                    return;
+                }
+            }
+
+            Debug.Log($"Built SerialMoveset for {graphName}!");
+
             try
             {
-                // Skip anything that isn't an asset file
-                if (!graphPath.EndsWith(".asset")) continue;
+                var writer = new BulkSerializeWriter();
+                serialMoveset.Write(writer);
+                using var fs = File.OpenWrite(outputFilePath);
+                using var sr = new StreamWriter(fs);
 
-                Debug.Log($"Reading graph...\n{graphPath}");
-
-                MovesetGraph graph = (MovesetGraph)AssetDatabase.LoadMainAssetAtPath(graphPath);
-                IdStateNode idStateNode = (IdStateNode)graph.nodes.Find(x => x.GetType() == typeof(IdStateNode));
-
-                serialMoveset.States.Add(idStateNode.GetData());
+                Debug.Log($"Writing file...\n{outputFilePath}!");
+                writer.Serialize(sr);
+                Debug.Log($"Finished writing file!\n{outputFilePath}!");
             }
             catch (Exception e)
             {
-                var idStateName = Path.GetFileNameWithoutExtension(graphPath);
-
-                Debug.LogError($"Error reading graph: {idStateName}\n{graphPath}");
+                Debug.LogError($"Error writing file!\n{outputFilePath}");
                 Debug.LogError(e.Message);
 
-                EditorUtility.DisplayDialog("NASB Moveset Editor", $"Export failed!\n\nError with IdState: {idStateName}\n\nSee error message in console window for more info.", "Close");
+                EditorUtility.DisplayDialog("NASB Moveset Editor", "Failed to write file!\n\nSee error message in console window for more info.", "Close");
                 return;
             }
+
+            EditorUtility.DisplayDialog("NASB Moveset Editor", $"Export complete!\n\nSaved to \"{outputFilePath}\"", "OK");
         }
 
-        Debug.Log($"Built SerialMoveset for {graphName}!");
-
-        try
+        private static void GenerateGraphs(SerialMoveset data, string itemName)
         {
-            var writer = new BulkSerializeWriter();
-            serialMoveset.Write(writer);
-            using var fs = File.OpenWrite(outputFilePath);
-            using var sr = new StreamWriter(fs);
+            string graphsFolderPath = Utils.GetGraphsDirPath();
+            if (!Directory.Exists(graphsFolderPath))
+                Directory.CreateDirectory(graphsFolderPath);
 
-            Debug.Log($"Writing file...\n{outputFilePath}!");
-            writer.Serialize(sr);
-            Debug.Log($"Finished writing file!\n{outputFilePath}!");
+            string updatedItemName = itemName;
+            string folderPath = Path.Combine(graphsFolderPath, itemName);
+
+            int duplicateIndex = 0;
+
+            while (Directory.Exists(folderPath))
+            {
+                ++duplicateIndex;
+                updatedItemName = itemName + "_" + duplicateIndex;
+                folderPath = Path.Combine(graphsFolderPath, updatedItemName);
+            }
+
+            AssetDatabase.CreateFolder(graphsFolderPath, updatedItemName);
+
+            string assetFolderPath = folderPath;
+
+            // Split the moveset apart by state
+            int normalizedCount = data.States.Count;
+            int i = 0;
+            foreach (IdState state in data.States)
+            {
+                ++i;
+                EditorUtility.DisplayProgressBar($"Importing {updatedItemName}...", $"Loading {state.Id}", i / data.States.Count);
+                ScriptableObject graph = ScriptableObject.CreateInstance("MovesetGraph");
+                AssetDatabase.CreateAsset(graph, $"{assetFolderPath}/{state.Id}.asset");
+
+                LoadMovesetIntoGraph(state, (MovesetGraph)graph, $"{assetFolderPath}/{state.Id}.asset");
+            }
+
+            EditorUtility.ClearProgressBar();
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
-        catch (Exception e)
+
+        private static void LoadMovesetIntoGraph(IdState data, MovesetGraph graph, string assetPath)
         {
-            Debug.LogError($"Error writing file!\n{outputFilePath}");
-            Debug.LogError(e.Message);
-
-            EditorUtility.DisplayDialog("NASB Moveset Editor", "Failed to write file!\n\nSee error message in console window for more info.", "Close");
-            return;
+            IdStateNode idStateNode = graph.AddNode<IdStateNode>();
+            AssetDatabase.AddObjectToAsset(idStateNode, assetPath);
+            idStateNode.SetData(data, graph, assetPath, Vector2.zero);
         }
-
-        EditorUtility.DisplayDialog("NASB Moveset Editor", $"Export complete!\n\nSaved to \"{outputFilePath}\"", "OK");
-    }
-
-    private static void GenerateGraphs(SerialMoveset data, string itemName)
-    {
-        string graphsFolderPath = Utils.GetGraphsDirPath();
-        if (!Directory.Exists(graphsFolderPath))
-            Directory.CreateDirectory(graphsFolderPath);
-
-        string updatedItemName = itemName;
-        string folderPath = Path.Combine(graphsFolderPath, itemName);
-
-        int duplicateIndex = 0;
-
-        while (Directory.Exists(folderPath))
-        {
-            ++duplicateIndex;
-            updatedItemName = itemName + "_" + duplicateIndex;
-            folderPath = Path.Combine(graphsFolderPath, updatedItemName);
-        }
-
-        AssetDatabase.CreateFolder(graphsFolderPath, updatedItemName);
-
-        string assetFolderPath = folderPath;
-
-        // Split the moveset apart by state
-        int normalizedCount = data.States.Count;
-        int i = 0;
-        foreach (IdState state in data.States)
-        {
-            ++i;
-            EditorUtility.DisplayProgressBar($"Importing {updatedItemName}...", $"Loading {state.Id}", i / data.States.Count);
-            ScriptableObject graph = ScriptableObject.CreateInstance("MovesetGraph");
-            AssetDatabase.CreateAsset(graph, $"{assetFolderPath}/{state.Id}.asset");
-
-            LoadMovesetIntoGraph(state, (MovesetGraph)graph, $"{assetFolderPath}/{state.Id}.asset");
-        }
-
-        EditorUtility.ClearProgressBar();
-
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-    }
-
-    private static void LoadMovesetIntoGraph(IdState data, MovesetGraph graph, string assetPath)
-    {
-        IdStateNode idStateNode = graph.AddNode<IdStateNode>();
-        AssetDatabase.AddObjectToAsset(idStateNode, assetPath);
-        idStateNode.SetData(data, graph, assetPath, Vector2.zero);
     }
 }
--- NASB Moveset Editor/Scripts/GraphHandlerWindow.cs
+++ NASB Moveset Editor/Scripts/GraphHandlerWindow.cs
@@ -6,96 +6,98 @@ using NASB_Parser;
 using System.IO;
 using System.Threading.Tasks;
 using XNode;
-using NASB_Moveset_Editor;
 using XNodeEditor;
 
-public class GraphHandlerWindow : EditorWindow
+namespace NASB_Moveset_Editor
 {
-    public string[] graphDirectories = { "None" };
-    public string[] graphNames = { "None" };
-    private int previousSelectionIndex = 0;
-    public int selectionIndex = 0;
-    public string selectedAssetName;
-
-    public void OnProjectChange()
+    public class GraphHandlerWindow : EditorWindow
     {
-        SetupGraphNames();
-    }
+        public string[] graphDirectories = { "None" };
+        public string[] graphNames = { "None" };
+        private int previousSelectionIndex = 0;
+        public int selectionIndex = 0;
+        public string selectedAssetName;
 
-    [MenuItem("NASB/NASB Moveset Editor", priority = 4)]
-    private static void OpenWindow()
-    {
-        GraphHandlerWindow window = GetWindow<GraphHandlerWindow>();
-        window.titleContent = new GUIContent(Consts.PROJECT_NAME + " " + Consts.VERSION);
-    }
-
-    private void OnEnable()
-    {
-        SetupGraphNames();
-        selectedAssetName = EditorPrefs.GetString(Consts.KEY_SELECTED_ASSET_NAME, "");
-
-        if (!selectedAssetName.Equals(string.Empty))
+        public void OnProjectChange()
         {
-            for (int i=0; i < graphNames.Length; ++i)
-            {
-                if (graphNames[i].Equals(selectedAssetName)) selectionIndex = i;
-            }
+            SetupGraphNames();
         }
-        else
-        {
-            Debug.Log($"{Consts.KEY_SELECTED_ASSET_NAME} was empty! Selecting first item in list.");
-        }
-    }
 
-    private void OnGUI()
-    {
-        EditorGUILayout.BeginHorizontal();
+        [MenuItem("NASB/NASB Moveset Editor", priority = 4)]
+        private static void OpenWindow()
         {
-            GUILayout.FlexibleSpace();
-            EditorGUILayout.BeginVertical(GUILayout.MaxWidth(400));
+            GraphHandlerWindow window = GetWindow<GraphHandlerWindow>();
+            window.titleContent = new GUIContent(Consts.PROJECT_NAME + " " + Consts.VERSION);
+        }
+
+        private void OnEnable()
+        {
+            SetupGraphNames();
+            selectedAssetName = EditorPrefs.GetString(Consts.KEY_SELECTED_ASSET_NAME, "");
+
+            if (!selectedAssetName.Equals(string.Empty))
             {
-                EditorGUILayout.Space(10);
-                EditorGUILayout.HelpBox("This is in Alpha! Things may break! You may lose all of your work!", MessageType.Warning);
-                EditorGUILayout.Space(10);
-                if (GUILayout.Button("Import TextAsset", GUILayout.MinHeight(35))) GraphHandler.LoadTextAsset();
-                EditorGUILayout.Space(10);
-                if (graphDirectories.Length > 0)
+                for (int i = 0; i < graphNames.Length; ++i)
                 {
-                    previousSelectionIndex = selectionIndex;
-                    selectionIndex = EditorGUILayout.Popup("Asset to export", selectionIndex, graphNames);
-                    if (selectionIndex != previousSelectionIndex)
-                    {
-                        // Selection changed, update prefs
-                        if (graphNames.Length > selectionIndex)
-                            EditorPrefs.SetString(Consts.KEY_SELECTED_ASSET_NAME, graphNames[selectionIndex]);
-                    }
-
-                    EditorGUILayout.Space();
-                    if (GUILayout.Button("Export TextAsset", GUILayout.MinHeight(50))) GraphHandler.SaveTextAsset(graphDirectories[selectionIndex], graphNames[selectionIndex]);
-                    EditorGUILayout.Space(10);
+                    if (graphNames[i].Equals(selectedAssetName)) selectionIndex = i;
                 }
-                EditorGUILayout.LabelField("Created by @megalon2d!");
-                EditorGUILayout.LabelField("NASB_Parser library originally created by sc2ad!");
-                EditorGUILayout.LabelField("xNode created by Siccity!");
             }
-            EditorGUILayout.EndVertical();
-            GUILayout.FlexibleSpace();
-        }
-        EditorGUILayout.EndHorizontal();
-    }
-
-    private void SetupGraphNames()
-    {
-        if (!Directory.Exists(Utils.GetGraphsDirPath()))
-        {
-            Directory.CreateDirectory(Utils.GetGraphsDirPath());
+            else
+            {
+                Debug.Log($"{Consts.KEY_SELECTED_ASSET_NAME} was empty! Selecting first item in list.");
+            }
         }
 
-        graphDirectories = Directory.GetDirectories(Utils.GetGraphsDirPath());
-        graphNames = new string[graphDirectories.Length];
-        for (int i = 0; i < graphDirectories.Length; ++i)
+        private void OnGUI()
         {
-            graphNames[i] = Path.GetFileName(graphDirectories[i]);
+            EditorGUILayout.BeginHorizontal();
+            {
+                GUILayout.FlexibleSpace();
+                EditorGUILayout.BeginVertical(GUILayout.MaxWidth(400));
+                {
+                    EditorGUILayout.Space(10);
+                    EditorGUILayout.HelpBox("This is in Alpha! Things may break! You may lose all of your work!", MessageType.Warning);
+                    EditorGUILayout.Space(10);
+                    if (GUILayout.Button("Import TextAsset", GUILayout.MinHeight(35))) GraphHandler.LoadTextAsset();
+                    EditorGUILayout.Space(10);
+                    if (graphDirectories.Length > 0)
+                    {
+                        previousSelectionIndex = selectionIndex;
+                        selectionIndex = EditorGUILayout.Popup("TextAsset to export", selectionIndex, graphNames);
+                        if (selectionIndex != previousSelectionIndex)
+                        {
+                            // Selection changed, update prefs
+                            if (graphNames.Length > selectionIndex)
+                                EditorPrefs.SetString(Consts.KEY_SELECTED_ASSET_NAME, graphNames[selectionIndex]);
+                        }
+
+                        EditorGUILayout.Space();
+                        if (GUILayout.Button("Export TextAsset", GUILayout.MinHeight(50))) GraphHandler.SaveTextAsset(graphDirectories[selectionIndex], graphNames[selectionIndex]);
+                        EditorGUILayout.Space(10);
+                    }
+                    EditorGUILayout.LabelField("Created by @megalon2d!");
+                    EditorGUILayout.LabelField("NASB_Parser library originally created by sc2ad!");
+                    EditorGUILayout.LabelField("xNode created by Siccity!");
+                }
+                EditorGUILayout.EndVertical();
+                GUILayout.FlexibleSpace();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void SetupGraphNames()
+        {
+            if (!Directory.Exists(Utils.GetGraphsDirPath()))
+            {
+                Directory.CreateDirectory(Utils.GetGraphsDirPath());
+            }
+
+            graphDirectories = Directory.GetDirectories(Utils.GetGraphsDirPath());
+            graphNames = new string[graphDirectories.Length];
+            for (int i = 0; i < graphDirectories.Length; ++i)
+            {
+                graphNames[i] = Path.GetFileName(graphDirectories[i]);
+            }
         }
     }
 }
