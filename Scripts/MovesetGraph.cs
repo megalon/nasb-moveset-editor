1c7b7a9c9c0212e9b123f0e05ac66d82f8826907
Megalon <megalongaming@gmail.com>
2021-11-30 19:04:49 -0800

    Remove asset menu option to create moveset graph

--- Scripts/MovesetGraph.cs
+++ Scripts/MovesetGraph.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 namespace NASB_Moveset_Editor
 {
-    [Serializable, CreateAssetMenu(fileName = "New Moveset Graph", menuName = "NASB Moveset Editor")]
+    [Serializable]
     public class MovesetGraph : NodeGraph {
         // public string happens = "testing";
 
