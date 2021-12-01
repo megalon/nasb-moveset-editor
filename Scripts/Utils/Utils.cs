81073535416b1f0f83a776a396c55f5c9279c205
Megalon <megalongaming@gmail.com>
2021-11-30 19:10:10 -0800

    Version bump. v0.2.0

--- Scripts/Utils/Utils.cs
+++ Scripts/Utils/Utils.cs
@@ -113,7 +113,7 @@ namespace NASB_Moveset_Editor
         public const float NodeVarHeight = 20f;
         public const int MaxDisplayNameLength = 12;
 
-        public const string VERSION = "0.1.7";
+        public const string VERSION = "0.2.0";
         public const string PROJECT_NAME = "NASB Moveset Editor";
         public const string GRAPH_FOLDER_NAME = "Moveset Graphs";
 
