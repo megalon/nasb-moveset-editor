bd85caa3d03119aa5d4eb9705302e67b403128c8
megalon <megalongaming@gmail.com>
2024-12-08 07:25:57 -0500

    Fix InputValidatorNode

--- Scripts/Nodes/Misc/InputValidatorNode.cs
+++ Scripts/Nodes/Misc/InputValidatorNode.cs
@@ -33,14 +33,20 @@ namespace NASB_Moveset_Editor.Misc
 		[Input(connectionType = ConnectionType.Override)] public InputValidator NodeInput;
 		public InputType InputType;
 		public bool RawX;
+		[Output(connectionType = ConnectionType.Override)] public FloatSourceContainer FloatValue;
+		public CtrlSeg Segment;
+		public FloatCompare FloatCompare;
+		public ButtonCompare ButtonCompare;
+		public CtrlSegCompare SegmentCompare;
+		public MultiCompare MultiCompare;
+		[Output(connectionType = ConnectionType.Multiple)] public InputValidator[] Validators;
 		
 		protected override void Init()
 		{
 			base.Init();
-
-			// TODO: Figure this out!
-			//// InputValidator should default to Inside, since zero is unused
-			//SegCompare = CtrlSegCompare.Inside;
+			ButtonCompare = ButtonCompare.Down;
+			SegmentCompare = CtrlSegCompare.Inside;
+			MultiCompare = MultiCompare.Any;
 		}
 		
 		public override object GetValue(NodePort port)
@@ -57,6 +63,30 @@ namespace NASB_Moveset_Editor.Misc
 			
 			InputType = data.InputType;
 			RawX = data.RawX;
+			FloatValue = data.FloatValue;
+			
+			FloatSourceContainerNode node_FloatValue = graph.AddNode<FloatSourceContainerNode>();
+			GetPort("FloatValue").Connect(node_FloatValue.GetPort("NodeInput"));
+			AssetDatabase.AddObjectToAsset(node_FloatValue, assetPath);
+			variableCount += node_FloatValue.SetData(FloatValue, graph, assetPath, nodeDepthXY + new UnityEngine.Vector2(1, variableCount));
+			++variableCount;
+			
+			Segment = data.Segment;
+			FloatCompare = data.FloatCompare;
+			ButtonCompare = data.ButtonCompare;
+			SegmentCompare = data.SegmentCompare;
+			MultiCompare = data.MultiCompare;
+			Validators = data.Validators;
+			
+			foreach (InputValidator Validators_item in data.Validators)
+			{
+				InputValidatorNode node_Validators = graph.AddNode<InputValidatorNode>();
+				GetPort("Validators").Connect(node_Validators.GetPort("NodeInput"));
+				AssetDatabase.AddObjectToAsset(node_Validators, assetPath);
+				variableCount += node_Validators.SetData(Validators_item, graph, assetPath, nodeDepthXY + new UnityEngine.Vector2(1, variableCount));
+				++variableCount;
+			}
+			
 			return variableCount;
 		}
 		
@@ -65,6 +95,24 @@ namespace NASB_Moveset_Editor.Misc
 			InputValidator objToReturn = new InputValidator();
 			objToReturn.InputType = InputType;
 			objToReturn.RawX = RawX;
+			if (GetPort("FloatValue").ConnectionCount > 0)
+			{
+				FloatSourceContainerNode FloatSourceContainer_Node = (FloatSourceContainerNode)GetPort("FloatValue").GetConnection(0).node;
+				objToReturn.FloatValue = FloatSourceContainer_Node.GetData();
+			}
+			objToReturn.Segment = Segment;
+			objToReturn.FloatCompare = FloatCompare;
+			objToReturn.ButtonCompare = ButtonCompare;
+			objToReturn.SegmentCompare = SegmentCompare;
+			objToReturn.MultiCompare = MultiCompare;
+			objToReturn.Validators = new InputValidator[GetPort("Validators").ConnectionCount];
+			int i = 0;
+			foreach(NodePort port in GetPort("Validators").GetConnections())
+			{
+				InputValidatorNode InputValidator_Node = (InputValidatorNode)port.node;
+				objToReturn.Validators[i] = InputValidator_Node.GetData();
+				++i;
+			}
 			return objToReturn;
 		}
 	}
